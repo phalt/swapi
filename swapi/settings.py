9448c8245638df210b1eb57e9ca2196c2370e1a0
Andrea Stagi <stagi.andrea@gmail.com>
2015-04-17 01:05:35 +0200

    Add WookieRenderer

--- swapi/settings.py
+++ swapi/settings.py
@@ -110,6 +110,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+        'resources.renderers.WookieRenderer'
     ),
     'PAGINATE_BY': 10,
     'DEFAULT_THROTTLE_CLASSES': (
