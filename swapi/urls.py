6dcffca5bbe86c37e76b4b1f3909d0f9fe71e8d9
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-16 19:37:26 +0300

    Replace url with path function, comment schema urls

--- swapi/urls.py
+++ swapi/urls.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
+
+from .views import index, documentation, about, stats, stripe_donation
+
 admin.autodiscover()
 
 from rest_framework import routers
@@ -17,19 +21,17 @@ router.register(r"species", views.SpeciesViewSet)
 router.register(r"vehicles", views.VehicleViewSet)
 router.register(r"starships", views.StarshipViewSet)
 
-
-urlpatterns = patterns("",
-    url(r"^admin/", include(admin.site.urls)),
-    url(r"^$", "swapi.views.index"),
-    url(r"^documentation$", "swapi.views.documentation"),
-    url(r"^about$", "swapi.views.about"),
-    url(r"^stats$", "swapi.views.stats"),
-    url(r"^stripe/donation", "swapi.views.stripe_donation"),
-    url(r"^api/people/schema$", "resources.schemas.people"),
-    url(r"^api/planets/schema$", "resources.schemas.planets"),
-    url(r"^api/films/schema$", "resources.schemas.films"),
-    url(r"^api/species/schema$", "resources.schemas.species"),
-    url(r"^api/vehicles/schema$", "resources.schemas.vehicles"),
-    url(r"^api/starships/schema$", "resources.schemas.starships"),
-    url(r"^api/", include(router.urls)),
-)
+urlpatterns = [
+    path("", index),
+    path("documentation", documentation),
+    path("about", about),
+    path("stats", stats),
+    path("stripe/donation", stripe_donation),
+    # url(r"^api/people/schema$", "resources.schemas.people"),
+    # url(r"^api/planets/schema$", "resources.schemas.planets"),
+    # url(r"^api/films/schema$", "resources.schemas.films"),
+    # url(r"^api/species/schema$", "resources.schemas.species"),
+    # url(r"^api/vehicles/schema$", "resources.schemas.vehicles"),
+    # url(r"^api/starships/schema$", "resources.schemas.starships"),
+    path("api/", include(router.urls)),
+]
