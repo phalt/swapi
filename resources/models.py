96b4c584baab0edec6671cb22e9303c14df22bd4
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-16 19:34:05 +0300

    Update ForeignKey`s with on_delete param

--- resources/models.py
+++ resources/models.py
@@ -69,7 +69,7 @@ class People(DateTimeModel):
 
     gender = models.CharField(max_length=40, blank=True)
 
-    homeworld = models.ForeignKey(Planet, related_name="residents")
+    homeworld = models.ForeignKey(Planet, related_name="residents", on_delete=models.PROTECT)
 
 
 class Transport(DateTimeModel):
@@ -148,7 +148,7 @@ class Species(DateTimeModel):
 
     average_lifespan = models.CharField(max_length=40)
 
-    homeworld = models.ForeignKey(Planet, blank=True, null=True)
+    homeworld = models.ForeignKey(Planet, blank=True, null=True, on_delete=models.PROTECT)
 
     language = models.CharField(max_length=40)
 
