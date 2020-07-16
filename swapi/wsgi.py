31305ac48fa53286b0234d9364fcb5eb4dadfbd5
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-16 19:35:16 +0300

    Remove DjangoWhiteNoise [deprecated]

--- swapi/wsgi.py
+++ swapi/wsgi.py
@@ -9,14 +9,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swapi.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-# Fix django closing connection to MemCachier after every request (#11331)
-from django.core.cache.backends.memcached import BaseMemcachedCache
-BaseMemcachedCache.close = lambda self, **kwargs: None
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swapi.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
